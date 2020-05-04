e6e954fcd5ffe0430424002f018e1bc591d9d22e
Paulo Victor Dos Santos Machados <paulo.machados@tivit.com>
2020-05-03 23:55:29 -0300

    'tela3'

--- AppADM/app/src/main/java/com/example/appadm/ADMTela2.kt
+++ AppADM/app/src/main/java/com/example/appadm/ADMTela2.kt
@@ -1,7 +1,9 @@
 package com.example.appadm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ADMTela2 : AppCompatActivity() {
 
@@ -9,4 +11,12 @@ class ADMTela2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a_d_m_tela2)
     }
+
+    fun irTela3(v:View){
+        val tela3 = Intent(this, ADMTela3::class.java)
+
+        startActivity(tela3)
+
+    }
+
 }

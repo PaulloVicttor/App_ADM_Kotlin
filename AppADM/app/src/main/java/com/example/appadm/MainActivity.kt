261c463c6f5cb960c4ac3d228a12f25dd937ad44
Paulo Victor Dos Santos Machados <paulo.machados@tivit.com>
2020-05-03 21:28:15 -0300

    segunda tela

---
+++ AppADM/app/src/main/java/com/example/appadm/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appadm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun irTela2(v:View){
+        val tela2 = Intent(this, ADMTela2::class.java)
+
+        startActivity(tela2)
+    }
+
+}

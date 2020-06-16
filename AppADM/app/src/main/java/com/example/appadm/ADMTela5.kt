8b85381024cfe9df249d7101be8aad03de2c3ad5
Paulo Victor Dos Santos Machados <paulo.machados@tivit.com>
2020-06-15 23:41:48 -0300

    'tela8'

--- AppADM/app/src/main/java/com/example/appadm/ADMTela5.kt
+++ AppADM/app/src/main/java/com/example/appadm/ADMTela5.kt
@@ -1,7 +1,9 @@
 package com.example.appadm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ADMTela5 : AppCompatActivity() {
 
@@ -9,4 +11,24 @@ class ADMTela5 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a_d_m_tela5)
     }
+
+    fun irTela6(v:View){
+        val tela6 = Intent(this,ADMTela6::class.java)
+
+        startActivity(tela6)
+
+    }
+
+    fun irTela7(v:View){
+        val tela7 = Intent(this, ADMTela7::class.java)
+
+        startActivity(tela7)
+    }
+
+    fun irTela8(v:View){
+        val tela8 = Intent(this, ADMTela8::class.java)
+
+        startActivity(tela8)
+    }
+
 }
---
+++ AppADM/app/src/main/java/com/example/appadm/ADMTela7.kt
@@ -0,0 +1,12 @@
+package com.example.appadm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ADMTela7 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a_d_m_tela7)
+    }
+}

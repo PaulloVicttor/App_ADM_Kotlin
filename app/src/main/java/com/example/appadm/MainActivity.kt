817cbd4fd5531a8a6f720cc50fe8a0c6e5d11493
Paulo Victor Dos Santos Machados <paulo.machados@tivit.com>
2020-07-01 00:58:10 -0300

    01/07

--- app/src/main/java/com/example/appadm/MainActivity.kt
+++ app/src/main/java/com/example/appadm/MainActivity.kt
@@ -1,23 +1,56 @@
 package com.example.appadm
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import com.example.appadm.rest.PlayerRestConfig
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
-fun doLogin(v:View){
 
-}
-    fun irTela2(v:View){
-        val tela2 = Intent(this, ADMTela2::class.java)
+    val usuario = etUsuario
 
-        startActivity(tela2)
+    val senha =  etSenha
+
+    fun irTela2(v: View) {
+        val tela2 = Intent(this, ADMTela3::class.java)
+
+
+    }
+
+
+    fun buttonLogin(v: View) {
+
+        //       Contexts: IO (network, disk), Main (UI), Default (heavy actions like filters operation)
+        CoroutineScope(Dispatchers.Main).launch {
+            getPlayersList()
+
+        }
+    }
+
+
+    private suspend fun getPlayersList() {
+//        withContext(Dispatchers.Main){
+        etUsuario.text = PlayerRestConfig.consult().getPlayerByNick(usuario).fullName
+//        }
+
     }
 
 }
--- app/src/main/java/com/example/appadm/rest/PlayerRestConfig.kt
+++ app/src/main/java/com/example/appadm/rest/PlayerRestConfig.kt
@@ -1,14 +1,31 @@
 package com.example.appadm.rest
 
+import android.os.StrictMode
+import com.example.appadm.PlayerEndpoints
 import feign.Feign
 import feign.gson.GsonDecoder
 import feign.gson.GsonEncoder
 
 object PlayerRestConfig {
-    fun createConnection(): PlayerEndpoints {
+
+    var connection: PlayerEndpoints? = null
+
+    private fun createConnection(): PlayerEndpoints {
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
         return Feign.builder()
             .decoder(GsonDecoder())
             .encoder(GsonEncoder())
             .target(PlayerEndpoints::class.java, "https://capivarawars-player-service.herokuapp.com")
     }
+
+    fun consult() : PlayerEndpoints {
+        if (connection == null){
+            connection = createConnection()
+        }
+
+        return connection as PlayerEndpoints
+    }
+
 }
